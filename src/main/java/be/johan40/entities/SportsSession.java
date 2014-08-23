b3056ed916ccf868d571e3e4a93632b88391e57f
“JDpiraat” <johandullaert@telenet.be>
2014-08-23 15:02:55 +0200

    rename Parcours to Track ; entity: SportSession and Sportsman

---
+++ src/main/java/be/johan40/entities/SportsSession.java
@@ -0,0 +1,22 @@
+package be.johan40.entities;
+
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.GregorianCalendar;
+
+public class SportsSession implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private long id;
+	private GregorianCalendar date;
+	// TODO check if there's a better suitable class for an interval input via
+	// frontend
+	private GregorianCalendar time;
+	private short heartbeatsPerMinute;
+	private short maxHeartbeatsPerMinute;
+	private short kilocalories;
+	private String remark;
+	private Track track;	
+	
+}
