e916887036b83d4784214039785033be5a2e93c4
“JDpiraat” <johandullaert@telenet.be>
2015-02-14 15:07:39 +0100

    enum's toString() implemented

--- src/main/java/be/johan40/enums/Activity.java
+++ src/main/java/be/johan40/enums/Activity.java
@@ -2,6 +2,16 @@ package be.johan40.enums;
 
 public enum Activity {
 
-	LOPEN, FIETSEN;
+	LOPEN("lopen"), FIETSEN("fietsen");
 
+	private final String string;
+
+	private Activity(String string) {
+		this.string = string;
+	}
+
+	@Override
+	public String toString() {
+		return string;
+	}
 }
--- src/main/java/be/johan40/enums/Status.java
+++ src/main/java/be/johan40/enums/Status.java
@@ -1,9 +1,17 @@
 package be.johan40.enums;
 
 public enum Status {
-	
-	DEFAULT,
-	EENMALIG,
-	INKEUZELIJST;
 
+	DEFAULT("default"), EENMALIG("eenmalig"), INKEUZELIJST("in keuzelijst");
+
+	private final String string;
+
+	private Status(String string) {
+		this.string = string;
+	}
+
+	@Override
+	public String toString() {
+		return string;
+	}
 }
